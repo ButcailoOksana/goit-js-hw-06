2bfdbd4743959bac1e4f4ee9728c8fb46187d168
ButcailoOksana <oksanabutcailo@gmail.com>
2022-08-06 17:48:12 +0300

    js 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const resultGetRandomHexColor = getRandomHexColor();
+
+const bodyEl = document.querySelector("body");
+const changeColorBtnEl = document.querySelector("button.change-color");
+// console.log(changeColorBtnEl);
+const spanColorEl = document.querySelector("span.color");
+// console.log(spanColorEl);
+
+changeColorBtnEl.addEventListener("click", setOutput);
+
+function setOutput() {
+  spanColorEl.textContent = `${getRandomHexColor()}`;
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+
+  console.log(`Background color -> ${getRandomHexColor()}`);
+}
