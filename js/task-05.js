28315c81c187236b68ee816848595d3f31df0ff7
ButcailoOksana <oksanabutcailo@gmail.com>
2022-08-07 17:06:50 +0300

    home work m6

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,5 @@
 const inputName = document.querySelector("#name-input");
-// console.log(formInputName);
 const outputName = document.querySelector("#name-output");
-// console.log(formOutputName);
 
 inputName.addEventListener("input", (event) => {
   outputName.textContent = event.currentTarget.value;
